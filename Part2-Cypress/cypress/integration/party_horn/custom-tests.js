da6cb3860e9fbae011b36d769eff3fe883fde5c5
thedestroyer4312 <trevortsai1@gmail.com>
2021-02-26 15:51:28 -0800

    Initial commit

---
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -0,0 +1,9 @@
+describe('Party Horn Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+
+  it('First Test', () => {
+    expect(true).to.equal(true);
+  });
+});
